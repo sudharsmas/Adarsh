34cd307264089c6f25948e8e6c1269642b62c96b
sudharsmas <sudharsmas@gmail.com>
2020-04-11 15:53:07 +0530

    file updated

--- pgm.java
+++ pgm.java
@@ -1,7 +1,7 @@
-class pgm
+class Pgm
 {
-public static void main(string args[])
+public static void main(String args[])
 {
-system.out.print("hello mello this is suson")
+System.out.print("hello mello this is suson")
 }
 }
